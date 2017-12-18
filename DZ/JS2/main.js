fbe4506baae457da3aa7cb64929fa1d79dea4d4e
Vadimenteractive <Vadimenteractive@mail.ru>
2017-12-18 07:52:59 +0200

    ява

--- DZ/JS2/main.js
+++ DZ/JS2/main.js
@@ -1,11 +1,13 @@
 function randomize(min, max) {
-    return Math.round(Math.random() * (max - min) +min);
+    return Math.round(Math.random() * (max - min) + min);
 }
 var minVar = prompt('Введите минимальное значение');
 var maxVar = prompt('Введите максимальное значение');
-var minParseType = typeof parseInt(minVar);
+var minParseType = typeof parseInt(minVar, 10);
 var maxParseType = typeof parseInt(maxVar);
-alert(typeof minParse);
-if (minParseType == 'number' && maxParseType  == 'number') {
-    console.log(randomize(parseInt(minVar), parseInt(maxVar)));
-}
+alert('minParse' + typeof minParseType);
+alert(parseInt(maxVar))
+alert(typeof minVar + minVar)
+// if (minParseType == 'number' && maxParseType  == 'number') {
+//     console.log(randomize(parseInt(minVar), parseInt(maxVar)));
+//}
