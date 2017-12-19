e3ab8605a63307b0db92b919a82e8a2dfff3b8ac
Vadimenteractive <Vadimenteractive@mail.ru>
2017-12-19 12:59:49 +0200

    ekzam

--- DZ/JS2/main.js
+++ DZ/JS2/main.js
@@ -3,11 +3,11 @@ function randomize(min, max) {
 }
 var minVar = prompt('Введите минимальное значение');
 var maxVar = prompt('Введите максимальное значение');
-var minParseType = typeof parseInt(minVar, 10);
+var minParseType = typeof parseInt(minVar);
 var maxParseType = typeof parseInt(maxVar);
 alert('minParse' + typeof minParseType);
 alert(parseInt(maxVar))
 alert(typeof minVar + minVar)
-// if (minParseType == 'number' && maxParseType  == 'number') {
-//     console.log(randomize(parseInt(minVar), parseInt(maxVar)));
-//}
+if (+minParseType == 'string' && +maxParseType  == 'string') {
+    alert('Ваше значение ' + randomize(+minVar, +maxVar));
+}
