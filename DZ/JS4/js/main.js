38bdedba8df22cbfb7557f5524ba210df02d52c9
vadimenteractive <vadimenteractive@mail.ru>
2017-12-30 17:56:35 +0200

    js

--- DZ/JS4/js/main.js
+++ DZ/JS4/js/main.js
@@ -1,5 +1,13 @@
 ;(function($){
-    $(document).on('keypress', function (e) {
-        console.log(e.key);
+    $(document).keydown(function(e) {
+        if (e.ctrlKey && e.keyCode == 13) {
+            alert("Вы нажали Ctrl+Enter!");
+        }
+
+    // $(document).on('keypress', function (e) {
+    //     if (e.ctrlKey) { console.log('ctrlKey');}
+    //
+    //     if (e.ctrlKey && e.key==a) {console.log('Урааа!');}
+
     })
 })(jQuery);
