8882c4f9510b76630f5140337ce6a02a320208ca
vadimenteractive <vadimenteractive@mail.ru>
2018-01-10 12:54:56 +0200

    js

--- DZ/JS4/js/main.js
+++ DZ/JS4/js/main.js
@@ -3,11 +3,27 @@
         if (e.ctrlKey && e.keyCode == 13) {
             alert("Вы нажали Ctrl+Enter!");
         }
+    });
+    $(document).on('keydown', function (e) {
 
-    // $(document).on('keypress', function (e) {
-    //     if (e.ctrlKey) { console.log('ctrlKey');}
-    //
-    //     if (e.ctrlKey && e.key==a) {console.log('Урааа!');}
 
+       if (e.ctrlKey && e.keyCode == 69) {
+           e.preventDefault();
+            $("div").replaceWith(function(index, oldHTML){
+                return $("<textarea>").html(oldHTML);
+            });
+
+        }
+        if (e.ctrlKey && e.keyCode == 83) {
+
+            $("textarea").replaceWith(function(index, oldHTML){
+                return $("<div>").html(oldHTML);
+            });
+
+        }
+
+    });
+    $(document).on('keydown', function (e) {
+        console.log('вы нажали ', e.keyCode);
     })
 })(jQuery);
