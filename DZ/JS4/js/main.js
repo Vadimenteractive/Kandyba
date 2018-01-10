54eba00da101fa0492fd9dfe7903e1294eb629e8
vadimenteractive <vadimenteractive@mail.ru>
2018-01-10 15:25:52 +0200

    js

--- DZ/JS4/js/main.js
+++ DZ/JS4/js/main.js
@@ -1,29 +1,32 @@
 ;(function($){
-    $(document).keydown(function(e) {
-        if (e.ctrlKey && e.keyCode == 13) {
-            alert("Вы нажали Ctrl+Enter!");
-        }
+    $(function(){
+        $(document).keydown(function(e) {
+            if (e.ctrlKey && e.keyCode == 13) {
+                alert("Вы нажали Ctrl+Enter!");
+            }
+        });
+        $(document).on('keydown', function (e) {
+
+
+            if (e.ctrlKey && e.keyCode == 69) {
+                e.preventDefault();
+                $("div").replaceWith(function(index, oldHTML){
+                    return $("<textarea>").html(oldHTML);
+                });
+
+            }
+            if (e.ctrlKey && e.keyCode == 83) {
+                e.preventDefault();
+                $("textarea").replaceWith(function(index, oldHTML){
+                    return $("<div>").html(oldHTML);
+                });
+
+            }
+
+        });
+        $(document).on('keydown', function (e) {
+            console.log('вы нажали ', e.keyCode);
+        });
     });
-    $(document).on('keydown', function (e) {
 
-
-       if (e.ctrlKey && e.keyCode == 69) {
-           e.preventDefault();
-            $("div").replaceWith(function(index, oldHTML){
-                return $("<textarea>").html(oldHTML);
-            });
-
-        }
-        if (e.ctrlKey && e.keyCode == 83) {
-
-            $("textarea").replaceWith(function(index, oldHTML){
-                return $("<div>").html(oldHTML);
-            });
-
-        }
-
-    });
-    $(document).on('keydown', function (e) {
-        console.log('вы нажали ', e.keyCode);
-    })
 })(jQuery);
