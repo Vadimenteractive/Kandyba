93c70d24cb39f1f9042bfa35f1a5f3e4255bef2d
vadimenteractive <vadimenteractive@mail.ru>
2018-01-17 17:13:53 +0200

    js

--- DZ/JS6/js/main.js
+++ DZ/JS6/js/main.js
@@ -1,17 +1,40 @@
 ;(function ($) {
     $(function () {
-$('.slide-contein').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    infinity: true,
-    dots: false,
-    slidesToShow: 2,
-    //centerMode: true,
-    slidesToScroll: 1,
-    //adaptiveHeight: true,
-    //centerPadding: '25px'
-    //variableWidth: true
-})
+        $('.slide-contein').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            infinity: true,
+            dots: false,
+            slidesToShow: 3,
+            centerMode: true,
+            slidesToScroll: 1,
+            //adaptiveHeight: true,
+            //centerPadding: '60px',
+            //variableWidth: true
+            responsive:[
+                {
+                    breakpoint: 1300,
+                    settings: {
+                        autoplay: false,
+                        autoplaySpeed: 2000,
+                        infinity: true,
+                        dots: false,
+                        slidesToShow: 2,
+                        centerMode: false,
+                        slidesToScroll: 1,
+                        //adaptiveHeight: true,
+                        //centerPadding: '60px',
+                        //variableWidth: true
+                    }
+                },
+                {
+                    breakpoint: 960,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        })
 
     });
 })(jQuery);
