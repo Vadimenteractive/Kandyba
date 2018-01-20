862267afbfe36fb6b5d205c226340c5510dafcee
Vadimenteractive <Vadimenteractive@mail.ru>
2018-01-20 22:32:05 +0200

    ekzam

--- DZ/js-sidebar/j.js
+++ DZ/js-sidebar/j.js
@@ -1,5 +1,21 @@
 ;(function($){
 $('aside').on('mouseenter', function () {
-        $(this).slide;
+    function sliU() {
+        $('.conteiner')
+            .slideDown(500);
+    };
+        $(this)
+            .animate({width: "250px"}, 500);
+    setTimeout(sliU, 600);
+
+    })
+    $('aside').on('mouseleave', function () {
+        function sliU() {
+            $('aside')
+                .animate({width: "15px"}, 500);
+        }
+        $('.conteiner')
+            .slideUp(500);
+        setTimeout(sliU, 600);
     })
 })(jQuery);
