dc28e6537d1c3da019bcc8ec132e10c350786917
Vadimenteractive <Vadimenteractive@mail.ru>
2018-02-03 22:40:15 +0200

    anim

--- lesons2/leson36-menu/js/main.js
+++ lesons2/leson36-menu/js/main.js
@@ -8,33 +8,102 @@
             var $ulMenu = $this.closest('#main-menu')
                 .children('ul');
             var $firstLi = $ulMenu
-                .children('li')
-                .eq(0);
-            function animateWidth() {
-                if ($firstLi.attr('data-class')) {
-                    var i;
-                    for (i=4; i<-1; i--) {
-                        $ulMenu
-                            .children('li')
-                            .eq(i)
-                            .removeAttr('data-class');
-                    }
-                }else {
-                    for (i=0; i<5; i++) {
-                        $ulMenu
-                            .children('li')
-                            .eq(i)
-                            .attr('data-class', 'open-li');
-                    }
-                }
+                .children('li');
+            var i = 0;
+
+
+            if ($ulMenu.attr('class') == "menu-opened") {
+
+                $firstLi
+                    .stop()
+                    .animate({'left': '-100%'}, 600, 'linear', function () {
+                        $this
+                            .closest('#main-menu')
+                            .children('ul')
+                            .removeClass('menu-opened')
+
+       //почему меню скрывается раньше завершения анимации
+
+                            .delay(1500)
+                            .hide();
+                    });
+            } else {
+
+                $this
+                    .closest('#main-menu')
+                    .children('ul')
+                    .addClass('menu-opened')
+                    .show();
+
+        //почему нельзя сделать всё по очереди
+
+                //while (i <5) {
+                    $firstLi.eq(i++)
+                        .delay(200)
+                        .animate({'left': '0'}, 500,
+                            function () {
+                                $firstLi.eq(i++)
+                                    .delay(200)
+                                    .animate({'left': '0'}, 500,
+                                        function () {
+                                            $firstLi.eq(i++)
+                                                .delay(200)
+                                                .animate({'left': '0'}, 500,
+                                                    function () {
+                                                        $firstLi.eq(i++)
+                                                            .delay(200)
+                                                            .animate({'left': '0'}, 500,
+                                                                function () {
+                                                                    $firstLi.eq(i++)
+                                                                        .delay(200)
+                                                                        .animate({'left': '0'}, 500,
+                                                                            function () {
+
+                                                                            });
+                                                                });
+                                                    });
+                                        });
+                            });
+               // }
             }
-            $this
-                .closest('#main-menu')
-                .children('ul')
-                .toggleClass('menu-opened')
-                .toggle(0, animateWidth());
 
 
         })
     })
 })(jQuery);
+
+// function animateWidth() {
+//     if ($firstLi.attr('data-class')) {
+//         var i;
+//         for (i=4; i<-1; i--) {
+//             $ulMenu
+//                 .children('li')
+//                 .eq(i)
+//                 .removeAttr('data-class');
+//         }
+//     }else {
+//         for (i=0; i<5; i++) {
+//             $ulMenu
+//                 .children('li')
+//                 .eq(i)
+//                 .attr('data-class', 'open-li');
+//         }
+//     }
+// }
+
+// $this
+//     .closest('#main-menu')
+//     .children('ul')
+//     .toggleClass('menu-opened')
+//     .toggle(0, function () {
+//         var i;
+//         for (i=0; i<5; i++) {
+//             $ulMenu
+//                 .children('li')
+//                 .eq(i)
+//                 .animate({'left': '0'}, 800)
+//                 //.attr('data-class', 'open-li')
+//                 .delay(500);
+//         }
+//     });
+
