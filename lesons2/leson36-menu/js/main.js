6ede08fda97218647d62779bd48505a43969b91b
Vadimenteractive <Vadimenteractive@mail.ru>
2018-02-03 16:53:29 +0200

    animate

--- lesons2/leson36-menu/js/main.js
+++ lesons2/leson36-menu/js/main.js
@@ -2,11 +2,38 @@
     $(function () {
         $('.hamburger').on('click', function () {
             var $this = $(this);
+            var $ulWidth = $this.closest('#main-menu')
+                .children('ul')
+                .width();
+            var $ulMenu = $this.closest('#main-menu')
+                .children('ul');
+            var $firstLi = $ulMenu
+                .children('li')
+                .eq(0);
+            function animateWidth() {
+                if ($firstLi.attr('data-class')) {
+                    var i;
+                    for (i=4; i<-1; i--) {
+                        $ulMenu
+                            .children('li')
+                            .eq(i)
+                            .removeAttr('data-class');
+                    }
+                }else {
+                    for (i=0; i<5; i++) {
+                        $ulMenu
+                            .children('li')
+                            .eq(i)
+                            .attr('data-class', 'open-li');
+                    }
+                }
+            }
             $this
                 .closest('#main-menu')
                 .children('ul')
                 .toggleClass('menu-opened')
-                .slideToggle(500);
+                .toggle(0, animateWidth());
+
 
         })
     })
