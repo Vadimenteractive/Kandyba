c3f05dd5c7f448f8f2bb111018442f3cc80f8f63
vadimenteractive <vadimenteractive@mail.ru>
2018-02-05 17:39:01 +0200

    js

--- lesons2/leson36-menu/js/main.js
+++ lesons2/leson36-menu/js/main.js
@@ -2,32 +2,45 @@
     $(function () {
         $('.hamburger').on('click', function () {
             var $this = $(this);
-            var $ulWidth = $this.closest('#main-menu')
+                 $ulWidth = $this.closest('#main-menu')
                 .children('ul')
-                .width();
-            var $ulMenu = $this.closest('#main-menu')
-                .children('ul');
-            var $firstLi = $ulMenu
-                .children('li');
-            var i = 0;
+                .width(),
+                 $ulMenu = $this.closest('#main-menu')
+                .children('ul'),
+                 $firstLi = $ulMenu
+                .children('li'),
+                 $liLenght = $firstLi.length,
+                 $openLiLenght = $ulMenu.find('[data-class="open-li"]').length,
+
+                 i = 0;
 
 
-            if ($ulMenu.attr('class') == "menu-opened") {
+            if ($openLiLenght == $liLenght) {
+console.log($openLiLenght + ' = ' + $liLenght);
+                animOut();
 
-                $firstLi
-                    .stop()
-                    .animate({'left': '-100%'}, 600, 'linear', function () {
+                function animOut() {
+                    $openLiLenght = $ulMenu.find('[data-class="open-li"]').length;
+                    if ($openLiLenght == 0) {
                         $this
                             .closest('#main-menu')
                             .children('ul')
                             .removeClass('menu-opened')
+                            .hide();
+                    } else {
+                        setTimeout(function () {
+                            $ulMenu
+                                .children('li')
+                                .eq(--$openLiLenght)
+                                .removeAttr('data-class');
+                            animOut();
+                        }, 150);
+                    }
+                };
 
-       //почему меню скрывается раньше завершения анимации
 
-                            .delay(0)
-                            .hide(600);
-                    });
-            } else {
+
+            } else if ($openLiLenght == 0){
 
                 $this
                     .closest('#main-menu')
@@ -35,30 +48,23 @@
                     .addClass('menu-opened')
                     .show();
 
-        //почему нельзя сделать всё по очереди
+
 anim();
-var delay = 0
-                //while (i <5) {
+
+
                 function anim(n) {
-                    if (i<5) {
+                    if (i<$liLenght) {
                         setTimeout(function () {
             $ulMenu
                 .children('li')
                 .eq(i++)
                 .attr('data-class', 'open-li');
-                            // $firstLi.eq(i++)
-                            //     .animate({'left': '0'}, 500, anim(delay));
-                            anim(n);}, 500);
-                        delay+=500;
 
+                            anim(n);
+                            }, 150);
                     }
-
                 }
-
-
             }
-
-
         })
     })
 })(jQuery);
