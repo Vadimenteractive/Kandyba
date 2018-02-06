a7ad94d32dc5c971ef064bf213e643420048d46f
vadimenteractive <vadimenteractive@mail.ru>
2018-02-06 13:22:49 +0200

    js

--- lesons2/leson36-menu/js/main.js
+++ lesons2/leson36-menu/js/main.js
@@ -3,16 +3,33 @@
 
         $('.hamburger').on('click', function () {
             var $this = $(this);
-                 $ulWidth = $this.closest('#main-menu')
-                .children('ul')
-                .width(),
+                 $ulWidth = $this
+                    .closest('#main-menu')
+                    .children('ul')
+                    .width(),
                  $ulMenu = $this.closest('#main-menu')
-                .children('ul'),
+                    .children('ul'),
                  $firstLi = $ulMenu
-                .children('li'),
+                    .children('li'),
                  $liLength = $firstLi.length,
-                 $openLiLength = $ulMenu.find('[data-class="open-li"]').length,
+                 $openLiLength = $ulMenu
+                     .find('[data-class="open-li"]')
+                     .length,
                  i = 0;
+
+            //функция очереди анимациий открытия элементов Li
+            function anim(n) {
+                if (i<$liLength) {
+                    setTimeout(function () {
+                        $ulMenu
+                            .children('li')
+                            .eq(i++)
+                            .attr('data-class', 'open-li');
+                        anim(n);
+                    }, 150);
+                }
+            }
+
 //функция очереди анимациий закрытия элементов Li
             function animOut() {
                 //помещаем в переменную колличество открытых элементов Li на данном этапе цикла
@@ -34,34 +51,23 @@
                         animOut();
                     }, 150);
                 }
-            };
-//функция очереди анимациий открытия элементов Li
-            function anim(n) {
-                if (i<$liLength) {
-                    setTimeout(function () {
-                        $ulMenu
-                            .children('li')
-                            .eq(i++)
-                            .attr('data-class', 'open-li');
-                        anim(n);
-                    }, 150);
-                }
             }
-
-//проверка на равное количество открытых элементов Li с общим колличеством элементов Li
-            if ($openLiLength == $liLength) {
-//запуск функции закрытия элементов Li
-                animOut();
 //проверка на то что нет открытых элементов Li
-            } else if ($openLiLength == 0){
+            if ($openLiLength == 0){
 
                 $this
                     .closest('#main-menu')
                     .children('ul')
                     .addClass('menu-opened')
                     .show();
-                //запуск функции Открытия элементов Li
-                anim();
+
+                anim(); //запуск функции Открытия элементов Li
+            }
+//проверка на равное количество открытых элементов Li с общим колличеством элементов Li
+            else if ($openLiLength == $liLength) {
+//запуск функции закрытия элементов Li
+                animOut();
+
             } else {i = $liLength;
                 animOut();
             }
